60f0009b61c1c505379512048884332068b4617a
Ronald Karunia <rkarunia@yahoo.com>
2013-07-10 06:17:28 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('./index.html');
+var length = buf.length;
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8',length));
 });
 
 var port = process.env.PORT || 5000;
